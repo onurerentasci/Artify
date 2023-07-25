091110ac1bbe23670774cd246b035706fe1d5ca1
onurerentasci <onurerentasci@gmail.com>
2023-07-25 23:42:27 +0300

    Artify initial commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import WelcomePage from "./screens/WelcomePage";
+import Signup from "./screens/Signup";
+import LoginPage from "./screens/LoginPage";
+import HomePage from "./screens/HomePage";
+import CreatePage from "./screens/CreatePage";
+import ProfilePage from "./screens/ProfilePage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => {
+  const navigation = useNavigation();
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Ionicons
+              name={route.name === "create" ? "search" : "person"}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tabs.Screen
+        name="create"
+        component={CreatePage}
+        options={{ headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        component={ProfilePage}
+      />
+    </Tabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TabsNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/CreatePage.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CreatePage = () => {
+  return (
+    <View>
+      <Text>CreatePage</Text>
+    </View>
+  );
+};
+
+export default CreatePage;
