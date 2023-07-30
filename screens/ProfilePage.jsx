45be7bdc5fcafe58d0337937ac33a9fc2d569c2e
onurerentasci <onurerentasci@gmail.com>
2023-07-30 23:49:15 +0300

    firestore connection added

--- screens/ProfilePage.jsx
+++ screens/ProfilePage.jsx
@@ -1,11 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { themeColors } from "../theme";
+import tw from "twrnc";
 
 const ProfilePage = () => {
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setEmail(user?.email ?? "");
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <View>
-      <SafeAreaView>
-        <Text>ProfilePage</Text>
+    <View style={[tw`flex-1`, { backgroundColor: themeColors.bg }]}>
+      <SafeAreaView style={[tw`flex-1 flex justify-around my-4 p-3`]}>
+        <ScrollView>
+          <Text>{email.split("@")[0]}</Text>
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
