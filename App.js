08faeaf009183301df29e683ce9640c62ba638ec
Onur Eren Taşcı <82609661+onurerentasci@users.noreply.github.com>
2023-08-13 22:11:52 +0300

    like func was removed

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import AppNavigation from "./navigation/AppNavigation";
 import "react-native-url-polyfill/auto";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <AppNavigation />
-    </SafeAreaProvider>
-  );
+  return <AppNavigation />;
 }
